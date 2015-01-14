8232186f1229f86189f3c2209b54b732e0c81274
Edgar Meinart <edgars@meinarts.name>
2015-01-14 18:15:30 +0200

    Removed grid from image

--- lib/kapchonka/utils.rb
+++ lib/kapchonka/utils.rb
@@ -24,35 +24,20 @@ module Kapchonka
 
     def self.generate_kapchonka_image_base64(captcha_key)
       final_image = Magick::Image.new(180, 90) { self.format = 'PNG' }
-      grid = Magick::Image.new(200, 200) { self.background_color = "Transparent"}
       text = Magick::Image.new(180, 90)
-
-      #Draw lines
-      grid_gc = Magick::Draw.new
-      grid_gc.stroke('red')
-      100.times do |i|
-        grid_gc.line(i * 10, 0, i * 10, 200)
-      end
-      grid_gc.stroke('blue')
-      100.times do |i|
-        grid_gc.line(0, i * 10, 200, i * 10)
-      end
-      grid_gc.draw(grid)
 
       # Draw Numbers
       gc = Magick::Draw.new
       gc.annotate(text, 0,0,0,0, captcha_key) do
         gc.gravity = CenterGravity
         self.pointsize = 25
-        self.font_family = "Arial"
+        self.font_family = 'Arial'
         self.font_weight = BoldWeight
-        self.stroke = "none"
+        self.stroke = 'transparent'
+        self.fill = 'black'
       end
 
-      grid.composite!(grid.rotate(rand(180)), CenterGravity, HardLightCompositeOp)
-      # grid.composite!(grid.rotate(rand(180)), CenterGravity, HardLightCompositeOp)
-      text.composite!(grid.rotate(rand(180)), CenterGravity, HardLightCompositeOp)
-      final_image.composite!(text, CenterGravity, HardLightCompositeOp)
+      final_image.composite!(text.rotate(rand(45)).wave( rand(50), 200 + rand(100)), CenterGravity, HardLightCompositeOp)
 
       Base64.encode64(final_image.to_blob).gsub("\n",'')
     end
